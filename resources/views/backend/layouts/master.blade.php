3bcf7cc22f4b477aaeb01b8285cb473fc6e4b6c4
DeepayanMallick <deepayan.cse@gmail.com>
2020-12-04 06:07:01 +0600

    Small Update

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Decor House | @yield('title')</title>
+    <title>Inventory Management | @yield('title')</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
